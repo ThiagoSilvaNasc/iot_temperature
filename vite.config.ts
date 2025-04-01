a4b1c20abffa16b236a9132d8f20a5efb96ec4d9
ThiagoSilvaNascimento <thiago3a@gmail.com>
2025-04-01 11:51:41 -0300

    Corrigindo base no vite e ajustando deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/bitbucket.org/electronics-projects-saframax/front_iot_temperature/',
+  base: '/iot_temperature/', // ⚠️ CONFIRME O NOME DO REPOSITÓRIO!
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
-  server: {
-      host:"0.0.0.0",
-      port:5173
-    },
 });
