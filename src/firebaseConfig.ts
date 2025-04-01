7b2a37082dc492228028b361aa48f9af5010f45b
ThiagoSilvaNascimento <thiago3a@gmail.com>
2025-04-01 11:19:37 -0300

    removendo chaves expostas

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -2,14 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getDatabase, ref, onValue } from "firebase/database";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDsXAAlvWQpoivQxpcJht9rh1aGglXDCW8",
-    authDomain: "esp32-309d2.firebaseapp.com",
-    databaseURL: "https://esp32-309d2-default-rtdb.firebaseio.com",
-    projectId: "esp32-309d2",
-    storageBucket: "esp32-309d2.firebasestorage.app",
-    messagingSenderId: "132334621417",
-    appId: "1:132334621417:web:defeb743b09d836dad5678"
-  };
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
+};
 
 const app = initializeApp(firebaseConfig);
 const database = getDatabase(app);
